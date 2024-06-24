575cfdaa6e5db45bfd2e3aa23a0e4ff76b4c2f4b
TanushKrashak <tankrashak38816@gmail.com>
2024-06-24 21:47:09 +0530

    Created Anim Instance Class in Cpp

---
+++ Source/Cpp_EndlessRunner/Public/Core/Cpp_AnimInstance_Player.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "Cpp_AnimInstance_Player.generated.h"
+
+
+UCLASS()
+class CPP_ENDLESSRUNNER_API UCpp_AnimInstance_Player : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
