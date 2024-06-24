4d9ed2cf2afeb34ee8626c74157b60fb0b06e2b1
TanushKrashak <tankrashak38816@gmail.com>
2024-06-24 21:34:25 +0530

    Add project files.

---
+++ Source/Cpp_EndlessRunner/Cpp_EndlessRunner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Cpp_EndlessRunner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Cpp_EndlessRunner, "Cpp_EndlessRunner" );
