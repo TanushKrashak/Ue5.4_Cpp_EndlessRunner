53060d0d9189c715e95ad5cf0428c6b75166826e
TanushKrashak <tankrashak38816@gmail.com>
2024-06-24 21:40:17 +0530

    Created Default Map + Gamemode

---
+++ Source/Cpp_EndlessRunner/Public/Core/Cpp_GM_EndlessRunner.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Cpp_GM_EndlessRunner.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_ENDLESSRUNNER_API ACpp_GM_EndlessRunner : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
