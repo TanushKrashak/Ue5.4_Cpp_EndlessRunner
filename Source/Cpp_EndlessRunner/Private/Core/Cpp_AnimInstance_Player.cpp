93c2cd51726bdc906245877b26065c919342a920
TanushKrashak <tankrashak38816@gmail.com>
2024-06-24 22:00:00 +0530

    Setup Pawn Speed, is in Air in Anim Instance

--- Source/Cpp_EndlessRunner/Private/Core/Cpp_AnimInstance_Player.cpp
+++ Source/Cpp_EndlessRunner/Private/Core/Cpp_AnimInstance_Player.cpp
@@ -2,4 +2,27 @@
 
 
 #include "Core/Cpp_AnimInstance_Player.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/Character.h"
 
+
+void UCpp_AnimInstance_Player::NativeInitializeAnimation() {
+	Super::NativeInitializeAnimation();
+	// Setup Pawn
+	if (Pawn == nullptr) {
+		Pawn = TryGetPawnOwner();		
+	}
+	// Setup Movement Component
+	if (Pawn) {
+		MovementComponent = Pawn->GetMovementComponent();
+	}
+}
+void UCpp_AnimInstance_Player::NativeUpdateAnimation(float DeltaSeconds) {
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	// Update Speed and bIsInAir
+	if (Pawn && MovementComponent) {
+		bIsInAir = MovementComponent->IsFalling();
+		Speed = MovementComponent->Velocity.Size();
+	}
+}
--- Source/Cpp_EndlessRunner/Public/Core/Cpp_AnimInstance_Player.h
+++ Source/Cpp_EndlessRunner/Public/Core/Cpp_AnimInstance_Player.h
@@ -6,10 +6,47 @@
 #include "Animation/AnimInstance.h"
 #include "Cpp_AnimInstance_Player.generated.h"
 
+// Forward Declarations
+class APawn;
+class UPawnMovementComponent;
 
 UCLASS()
 class CPP_ENDLESSRUNNER_API UCpp_AnimInstance_Player : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+
+public:
+	//================================================================================================================
+	// PROPERTIES & VARIABLES
+	//================================================================================================================
+
+
+	//================================================================================================================
+	// FUNCTIONS
+	//================================================================================================================
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+
+
+protected:
+	//================================================================================================================
+	// PROPERTIES & VARIABLES
+	//================================================================================================================
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Movement")
+	float Speed;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Movement")
+	bool bIsInAir;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Movement")
+	APawn* Pawn;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Movement")
+	UPawnMovementComponent* MovementComponent;
+
+	//================================================================================================================
+	// FUNCTIONS
+	//================================================================================================================
 };
